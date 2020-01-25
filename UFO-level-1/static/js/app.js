78c58dce4c999828ce19276d30a6539e2541fe05
Damir <54745928+dgalilullin@users.noreply.github.com>
2020-01-25 16:58:07 -0500

    Update app.js

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -1,46 +1,53 @@
 // from data.js
 var tableData = data;
 
-// YOUR CODE HERE!
 // Get a reference to the table body
 var tbody = d3.select("tbody");
+tbody.html("");
 
-
-var inputField = d3.select("#datetime");
-
-var button = d3.select("#filter-btn");
-
-inputField.on("change", function() {
-    var newDate = d3.event.target.value;
-    console.log(newDate);
-  });
-  
-
-function handleClick() {
-    console.log("A button was clicked!");
-    console.log(d3.event.target);
-  }
-
-button.on("click", handleClick);  
-
-
-button.on("click", function() {
-    console.log("Hi, a button was clicked!");
-    console.log(d3.event.target);
-  });
-  
-
-//var selecteddate = '1/1/2010'
-
-var filteredData = tableData.filter(filterdate => filterdate.datetime == inputField);
-
-  console.log(filteredData);
-
-
-filteredData.forEach((a) => {
+tableData.forEach((a) => {
   var row = tbody.append("tr");
   Object.entries(a).forEach(([key, value]) => {
     var cell = row.append("td");
     cell.text(value);
   });
 });
+
+var button = d3.select("#filter-btn");
+
+button.on("click", function() {
+    d3.event.preventDefault();
+    var inputField = d3.select("#datetime");
+    var inputValue = inputField.property("value");
+    var selectData = tableData.filter(date1=>date1.datetime === inputValue);
+    console.log(selectData)
+
+    var tbody = d3.select("tbody");
+    tbody.html("");
+    selectData.forEach((a) => {
+      var row = tbody.append("tr");
+      Object.entries(a).forEach(([key, value]) => {
+        var cell = row.append("td");
+        cell.text(value);
+      });
+    });
+});
+
+// inputField.on("change", function() {
+//     var newDate = d3.event.target.value;
+//     console.log(newDate);
+//   });
+  
+
+// function handleClick() {
+//     console.log("A button was clicked!");
+//     console.log(d3.event.target);
+//   }
+
+// button.on("click", handleClick);  
+
+
+
+  
+
+
